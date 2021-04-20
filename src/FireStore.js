15c3a8006e3e9d32da345f645751cbf6f35e18d1
Ying Gu <yinggu@Yings-MacBook-Pro.local>
2021-04-21 00:54:04 +0200

    changed the FireStore.js data

--- src/FireStore.js
+++ src/FireStore.js
@@ -1,10 +1,14 @@
-import firebase from "firebase";
+import firebase from "firebase"
 
 const db = firebase
   .initializeApp({
-    projectId: "YOUR_PROJECT_ID",
-    databaseURL: "YOUR_DB_URL"
+    apiKey: "AIzaSyBtpL2NR1zGGurWidGirR6w1zOeU7xOIGM",
+    authDomain: "whatsappclone-31816.firebaseapp.com",
+    projectId: "whatsappclone-31816",
+    storageBucket: "whatsappclone-31816.appspot.com",
+    messagingSenderId: "939099947977",
+    appId: "1:939099947977:web:d05fdb239c1fc31d3fde18",
   })
-  .firestore();
+  .firestore()
 
-export default db;
+export default db
